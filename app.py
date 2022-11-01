a52d4c205c8b5ee175c84b7d98d6eb0f7ebf1df6
CKrikas <chrisskrikas@gmail.com>
2022-11-01 05:47:23 +0200

    minimal flask testing

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello World"
+
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+    if(request.method == 'GET'):
+  
+        data = "hello world"
+        return jsonify({'data': data})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="localhost", port=8000, debug=True)
